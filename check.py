592d0c36e57be04383258fe2102e165c330cf55e
RomoR11 <rm.mikhanov@gmail.com>
2024-02-15 18:26:35 +0300

    Тренировки в полёте

--- check.py
+++ check.py
@@ -26,5 +26,10 @@ def promotion_image():
     return render_template('promotion_image.html')
 
 
+@app.route('/training/<prof>')
+def profession(prof):
+    return render_template('profession.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
